eed2a11792850a15cb8b757c359d84065e4ee69f
Christopher <ckaliszewski@gmail.com>
2024-08-03 04:09:16 -0400

    build completed and tested

--- MyHitbox.ino
+++ MyHitbox.ino
@@ -1,129 +1,121 @@
 #include <Keyboard.h>
 
-#define UP_BUTTON_PIN       2
-#define DOWN_BUTTON_PIN     3
-#define LEFT_BUTTON_PIN     4
-#define RIGHT_BUTTON_PIN    5
-#define X_BUTTON_PIN        6
-#define Y_BUTTON_PIN        7
-#define A_BUTTON_PIN        8
-#define B_BUTTON_PIN        9
-#define R1_BUTTON_PIN      10
-#define R2_BUTTON_PIN      11
-#define L1_BUTTON_PIN      12
-#define L2_BUTTON_PIN      13
-#define EXTRA_1_BUTTON_PIN 14
-#define EXTRA_2_BUTTON_PIN 15
-#define EXTRA_3_BUTTON_PIN 16
+#define UP_BUTTON_PIN      16
+#define DOWN_BUTTON_PIN     9
+#define LEFT_BUTTON_PIN    10
+#define RIGHT_BUTTON_PIN    8
+#define X_BUTTON_PIN        3
+#define Y_BUTTON_PIN        2
+#define A_BUTTON_PIN        7
+#define B_BUTTON_PIN        6
+#define R1_BUTTON_PIN       0
+#define R2_BUTTON_PIN       5
+#define L1_BUTTON_PIN       1
+#define L2_BUTTON_PIN       4
+#define EXTRA_1_BUTTON_PIN 19
+#define EXTRA_2_BUTTON_PIN 20
+#define EXTRA_3_BUTTON_PIN 21
 
+#define UP_BUTTON_ASCII      119  // w
+#define DOWN_BUTTON_ASCII    115  // s
+#define LEFT_BUTTON_ASCII     97  // a
+#define RIGHT_BUTTON_ASCII   100  // d
+#define X_BUTTON_ASCII       117  // u
+#define Y_BUTTON_ASCII       105  // i
+#define A_BUTTON_ASCII       106  // j
+#define B_BUTTON_ASCII       107  // k
+#define R1_BUTTON_ASCII      111  // o
+#define R2_BUTTON_ASCII      108  // l
+#define L1_BUTTON_ASCII      112  // p
+#define L2_BUTTON_ASCII       59  // ;
+#define EXTRA_1_BUTTON_ASCII  10  // (enter) line feed
+#define EXTRA_2_BUTTON_ASCII  92  // (\) backslash
+#define EXTRA_3_BUTTON_ASCII   8  // backspace
+
+#define LOG_THRESHOLD_MS 5000
+
+struct Button
+{
+    unsigned int Pin;
+    unsigned int AsciiOutput;
+    unsigned int State;
+    unsigned int PreviousState;
+};
+
+Button buttons[15] =
+{
+    { UP_BUTTON_PIN     , UP_BUTTON_ASCII     , HIGH, HIGH },
+    { DOWN_BUTTON_PIN   , DOWN_BUTTON_ASCII   , HIGH, HIGH },
+    { LEFT_BUTTON_PIN   , LEFT_BUTTON_ASCII   , HIGH, HIGH },
+    { RIGHT_BUTTON_PIN  , RIGHT_BUTTON_ASCII  , HIGH, HIGH },
+    { X_BUTTON_PIN      , X_BUTTON_ASCII      , HIGH, HIGH },
+    { Y_BUTTON_PIN      , Y_BUTTON_ASCII      , HIGH, HIGH },
+    { A_BUTTON_PIN      , A_BUTTON_ASCII      , HIGH, HIGH },
+    { B_BUTTON_PIN      , B_BUTTON_ASCII      , HIGH, HIGH },
+    { R1_BUTTON_PIN     , R1_BUTTON_ASCII     , HIGH, HIGH },
+    { R2_BUTTON_PIN     , R2_BUTTON_ASCII     , HIGH, HIGH },
+    { L1_BUTTON_PIN     , L1_BUTTON_ASCII     , HIGH, HIGH },
+    { L2_BUTTON_PIN     , L2_BUTTON_ASCII     , HIGH, HIGH },
+    { EXTRA_1_BUTTON_PIN, EXTRA_1_BUTTON_ASCII, HIGH, HIGH },
+    { EXTRA_2_BUTTON_PIN, EXTRA_2_BUTTON_ASCII, HIGH, HIGH },
+    { EXTRA_3_BUTTON_PIN, EXTRA_3_BUTTON_ASCII, HIGH, HIGH },
+};
+
+unsigned int buttonsCount;
+
+Button* upButtonPtr    = &buttons[0];
+Button* downButtonPtr  = &buttons[1];
+Button* leftButtonPtr  = &buttons[2];
+Button* rightButtonPtr = &buttons[3];
+
+unsigned int tempLeftButtonState = HIGH;
 
 void setup()
 {
-    pinMode(UP_BUTTON_PIN     , INPUT_PULLUP);
-    pinMode(DOWN_BUTTON_PIN   , INPUT_PULLUP);
-    pinMode(LEFT_BUTTON_PIN   , INPUT_PULLUP);
-    pinMode(RIGHT_BUTTON_PIN  , INPUT_PULLUP);
-    pinMode(X_BUTTON_PIN      , INPUT_PULLUP);
-    pinMode(Y_BUTTON_PIN      , INPUT_PULLUP);
-    pinMode(A_BUTTON_PIN      , INPUT_PULLUP);
-    pinMode(B_BUTTON_PIN      , INPUT_PULLUP);
-    pinMode(R1_BUTTON_PIN     , INPUT_PULLUP);
-    pinMode(R2_BUTTON_PIN     , INPUT_PULLUP);
-    pinMode(L1_BUTTON_PIN     , INPUT_PULLUP);
-    pinMode(L2_BUTTON_PIN     , INPUT_PULLUP);
-    pinMode(EXTRA_1_BUTTON_PIN, INPUT_PULLUP);
-    pinMode(EXTRA_2_BUTTON_PIN, INPUT_PULLUP);
-    pinMode(EXTRA_3_BUTTON_PIN, INPUT_PULLUP);
+    buttonsCount = sizeof(buttons) / sizeof(buttons[0]);
+
+    for (unsigned int i = 0; i < buttonsCount; ++i)
+    {
+        pinMode(buttons[i].Pin, INPUT_PULLUP);
+    }
 
     Keyboard.begin();
-
     Serial.begin(9600);
-    Serial.println("Board has been initialized.");
 }
 
 void loop()
 {
+    // set all buttons current state
+    for (unsigned int i = 0; i < buttonsCount; i++)
+    {
+        buttons[i].State = digitalRead(buttons[i].Pin);
+    }
+
     // vertical Simultaneous Opposing Cardinal Directions (SOCD) filter
-    if (digitalRead(UP_BUTTON_PIN) == LOW)
-    {
-        Keyboard.write('w');
-    }
-    else if (digitalRead(DOWN_BUTTON_PIN) == LOW)
-    {
-        Keyboard.write('s');
-    }
+    downButtonPtr->State = downButtonPtr->State == LOW && upButtonPtr->State == HIGH ? LOW : HIGH;
 
     // horizontal SOCD filter
-    if (digitalRead(LEFT_BUTTON_PIN) == LOW && digitalRead(RIGHT_BUTTON_PIN) == HIGH)
+    tempLeftButtonState = leftButtonPtr->State == LOW && rightButtonPtr->State == LOW
+        ? HIGH : leftButtonPtr->State;
+    rightButtonPtr->State = rightButtonPtr->State == LOW && leftButtonPtr->State == LOW
+        ? HIGH : rightButtonPtr->State;
+    leftButtonPtr->State = tempLeftButtonState;
+
+    for (unsigned int i = 0; i < buttonsCount; i++)
     {
-        Keyboard.write('a');
+        if (buttons[i].State == LOW && buttons[i].State != buttons[i].PreviousState)
+        {
+            Keyboard.press(buttons[i].AsciiOutput);
+        }
+        else if (buttons[i].State == HIGH)
+        {
+            Keyboard.release(buttons[i].AsciiOutput);
+        }
     }
 
-    // horizontal SOCD filter
-    if (digitalRead(RIGHT_BUTTON_PIN) == LOW && digitalRead(LEFT_BUTTON_PIN) == HIGH)
+    // set all buttons previous state
+    for (unsigned int i = 0; i < buttonsCount; i++)
     {
-        Keyboard.write('d');
-    }
-
-    if (digitalRead(X_BUTTON_PIN) == LOW)
-    {
-        Keyboard.write('u');
-    }
-
-    if (digitalRead(Y_BUTTON_PIN) == LOW)
-    {
-        Keyboard.write('i');
-    }
-
-    if (digitalRead(A_BUTTON_PIN) == LOW)
-    {
-        Keyboard.write('j');
-    }
-
-    if (digitalRead(B_BUTTON_PIN) == LOW)
-    {
-        Keyboard.write('k');
-    }
-
-    if (digitalRead(R1_BUTTON_PIN) == LOW)
-    {
-        Keyboard.write('o');
-    }
-
-    if (digitalRead(R2_BUTTON_PIN) == LOW)
-    {
-        Keyboard.write('l');
-    }
-
-    if (digitalRead(L1_BUTTON_PIN) == LOW)
-    {
-        Keyboard.write('p');
-    }
-
-    if (digitalRead(L2_BUTTON_PIN) == LOW)
-    {
-        Keyboard.write(';');
-    }
-
-    if (digitalRead(L2_BUTTON_PIN) == LOW)
-    {
-        Keyboard.write(';');
-    }
-
-    if (digitalRead(EXTRA_1_BUTTON_PIN) == LOW)
-    {
-        // press Enter key
-        Keyboard.println();
-    }
-
-    if (digitalRead(EXTRA_2_BUTTON_PIN) == LOW)
-    {
-        Keyboard.write('\\');
-    }
-
-    if (digitalRead(EXTRA_3_BUTTON_PIN) == LOW)
-    {
-        // backspace ASCII code
-        Keyboard.write(8);
+        buttons[i].PreviousState = buttons[i].State;
     }
 }
--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,10 @@
 # My Hitbox
+&nbsp; | &nbsp; | &nbsp;
+--- | --- | ---
+![](images/Open.jpg) | ![](images/Front.jpg) | ![](images/Back.jpg)
+
 ## Hardware Used
+- [1x] Arduino Pro Micro
 - [18x] 3 x 8 mm round magnets
 - [15x] Kailh Choc Low Profile Switches (v1)
 
@@ -27,3 +32,28 @@ The steps below are for flashing firmware to an Arduino Pro Micro (Arduino Micro
 - Build HEX file in **Arduino IDE** by navigating to **Sketch** > **Export Compiled Binary**.
     - Alt + Ctrl + S
     - HEX file needed is located at `build/arduino.avr.micro/MyHitbox.ino.hex`.
+
+## Side Quests
+### Flashing Corrupted Bootloader
+While I was learning about the Arduino Pro Micro's firmware upload process I mistakenly corrupted the Caterina bootloader, bricking the controller. This led me to learn about [Arduino as In-circuit Serial Programmers (ISP)](https://docs.arduino.cc/built-in-examples/arduino-isp/ArduinoISP) which allows for programming of the whole flash memory of the microcontroller, bootloader included, through a specific set of pins connected to a separate programmer board. I had an [ESPWROOM32](https://www.espressif.com/sites/default/files/documentation/esp32-wroom-32_datasheet_en.pdf) laying around to use as a programmer board.
+
+#### Steps Taken
+1. In the **Arduino IDE** load the **ArduinoISP** example. It can be found under **File** > **Examples** > **11. Arduino ISP** > **ArduinoISP**.
+1. Update the **RESET**, **LED_HB**, **LED_ERR** and **LED_PMODE** pins to be 22, 32, 33 and 25 respectively.
+    - **RESET** is originally set to GPIO8 but on the ESP32 pin 8 is for internal SDI. When this pin is used the board enters a bootloop with a TG1WDT_SYS_RESET error.
+    - **LED_HB**, **LED_ERR** and **LED_PMODE** are also set to internal pins and need to be reassigned to available output pins to avoid the bootloop.
+1. Upload the modified **ArduinoISP** sketch to the ESP32.
+1. Hook up the ISP wires between the programmer and target board according to the [Pin Map](#pin-map).
+1. Update the target board to be **Arduino Micro**. It can be set under **Tools** > **Board** > **Arduino AVR Boards** > **Arduino Micro**.
+1. Set the programmer to be **Arduino as ISP**. It can be found under **Tools** > **Programmer** > **Arduino as ISP**.
+1. Burn the bootloader to the target board using the programmer. It can be done from **Tools** > **Burn Bootloader**.
+
+#### Pin Map
+Description | ESP32 Pin | Arduino Micro Pin
+--- | --- | ---
+VCC | VIN | VCC
+GND | GND | GND
+MISO | 19 | 14
+MOSI | 23 | 16
+SCK | 18 | 15
+RESET | 22 | RST
